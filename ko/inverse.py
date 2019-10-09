9b61bd56638d712fb62afd96f144e3d1d1fcfc96
tokey <tokey0219@gmail.com>
2019-10-09 12:13:39 +0900

    add new file

---
+++ ko/inverse.py
@@ -0,0 +1,14 @@
+# 数値計算用のライブラリとしてnumpyをnpとしてインポート
+import numpy as np
+
+# 行列Aを定義
+A = np.array([[1,3,2],[-1,0,1],[2,3,0]])
+print(f"A:\n{A}\n")
+
+# 逆行列を計算
+A_inv = np.linalg.inv(A)
+print(f"A_inv:\n{A_inv}\n")
+
+# 行列Aと逆行列A_invの積
+AA_inv = np.matmul(A,A_inv)
+print(f"A * A_inv:\n{AA_inv}\n")
