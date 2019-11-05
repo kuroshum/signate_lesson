2caebf20fbb7a8d6efdee9c7966021f2dafcc51b
tokey <tokey0219@gmail.com>
2019-11-05 12:10:10 +0900

    update file

--- ko/leastSquares.py
+++ ko/leastSquares.py
@@ -1,19 +1,17 @@
 import numpy as np
 import regressionData as rg
 
-def train(self, dataType):
-    X = self.xTrain
-    Xprime = np.vstack([X.T, np.ones(dataType, dtype = np.int)]) # Xprime = [x1, x2, ..., xN, 1].T
+def train(self):
+    Xprime = np.vstack([self.xTrain, np.ones(self.yTrain.shape, dtype = np.int)])
     XprimeT = Xprime.T
-    Y = np.append(self.yTrain, 1)
-    inv_sum_xx = np.sum(np.matmul(Xprime, XprimeT), dtype = np.double) ** -1
-    yx = np.matmul(Y, Xprime)
-    return inv_sum_xx * yx
+    inv_sum_xx = np.linalg.inv(np.matmul(Xprime, XprimeT))
+    yx = np.matrix(np.matmul(self.yTrain, XprimeT)).T
+    return np.matmul(inv_sum_xx, yx)
 
 # データの生成
 myData1 = rg.artificial(200,100,dataType="1D")
 myData2 = rg.artificial(200,100,dataType="2D")
-w1 = train(myData1, 1)
-print(f"w1:\n{w1}\n")
-w2 = train(myData2, 2)
-print(f"w2:\n{w2}\n")
+w1 = train(myData1)
+print(f"w1:\n{w1}\n") # 一行目：W1 二行目：b
+w2 = train(myData2)
+print(f"w2:\n{w2}\n") # 一行目、二行目：W1,W2 三行目：b
